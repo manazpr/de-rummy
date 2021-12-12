fa6ab2be38388b90cfa61d9114afa35b5230bd9f
Manaz PR <manas0096@gmail.com>
2021-12-12 23:02:06 +0530

    commit

--- x/app.js
+++ x/app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const http = require('http');
 const WebSocket = require('ws');
-const Game = require('./game');
+const Game = require('../game');
 
 const app = express();
 
