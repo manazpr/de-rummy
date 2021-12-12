86f34ca2d7f4efc50e4494af40f3cf872b124a65
Manaz PR <manas0096@gmail.com>
2021-12-12 23:15:50 +0530

    commit

--- app.js
+++ app.js
@@ -1,21 +1,12 @@
 const express = require('express')
 const http = require('http');
-const https = require('https');
 const WebSocket = require('ws');
-const fs = require('fs');
 const Game = require('./game');
 
 const app = express();
 
-// SSL Files
-const sslOptions = {
-  cert: fs.readFileSync('./fullchain.pem'),
-  key: fs.readFileSync('./privkey.pem')
-};
-
 const server = http.createServer(app);
-const secureServer = https.createServer(sslOptions, app)
-const wss = new WebSocket.Server({ server: secureServer });
+const wss = new WebSocket.Server({ server });
 const rummy = new Game(wss);
 
 // Serve Static Files/Assets
@@ -27,7 +18,7 @@ wss.on('close', () => console.log('*disconnected*'));
 
 /*----------------------ENDPOINTS----------------------*/
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/index.html');
+  res.sendFile(__dirname + '/public/newgame.html');
 });
 
 app.get('/join/:lobby', (req, res) => {
@@ -35,7 +26,7 @@ app.get('/join/:lobby', (req, res) => {
   if (rummy.addLobby(code)) {
     res.redirect('/game/' + req.params.lobby + '/' + rummy.lobbys[code].token);
   } else {
-    res.redirect('/');
+    res.redirect('/newgame/');
   }
 });
 
@@ -44,7 +35,7 @@ app.get('/joincpu/:lobby', (req, res) => {
   if (rummy.addLobby(code, cpu=true)) {
     res.redirect('/game/' + req.params.lobby + '/' + rummy.lobbys[code].token);
   } else {
-    res.redirect('/');
+    res.redirect('/newgame/');
   }
 });
 
@@ -54,7 +45,7 @@ app.get('/game/:lobby/:token', (req, res) => {
   if (req.params.token && rummy.lobbys[code] && rummy.lobbys[code].token == token) {
     res.sendFile(__dirname + '/public/game.html');
   } else {
-    res.redirect('/');
+    res.redirect('/newgame/');
   }
 });
 /*-----------------------------------------------------*/
@@ -62,7 +53,4 @@ app.get('/game/:lobby/:token', (req, res) => {
 // Start Server
 server.listen(5000, () => {
   console.log('Listening on port 5000...')
-});
-secureServer.listen(443, () => {
-  console.log('(Secure) Listening on port 443...')
 });
